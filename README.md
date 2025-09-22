7a255bebdf5af1de6cb54bc475ab5e4edc27bca8
DanishCraftYT <email@example.com>
2025-09-22 11:45:10 +0200

    added a info content box. started working on a improved details HTML element for the wiki. updated README.md file.

--- README.md
+++ README.md
@@ -5,4 +5,4 @@ a simple HTML, CSS, and Javascript framework for making basic wikies that look l
 * test images to make sure they are compatible with the framework.<br>
 * consider adding a "wiki-headersubsection" for grouping multiple header items together.<br>
 * consider making a custom "details" element which works like the one from fandom wikies.<br>
-* a info box similar to "wiki-tablecontent".<br>
+* consider combining "wiki-tablecontent" and "wiki-infocontent" elements into a "wiki-content" element which will have a attribute for it's top text.<br>
--- WikiWare.js
+++ WikiWare.js
@@ -33,7 +33,14 @@ class WikiTableContent extends HTMLElement {
     }
 }
 
-class WikiTableContentSection extends HTMLElement {
+class WikiInfoContent extends HTMLElement {
+    constructor() {
+        super();
+        this.insertAdjacentHTML("afterbegin", "<h2>Info</h2><hr>");
+    }
+}
+
+class WikiContentSection extends HTMLElement {
     constructor() {
         super();
         if (this.getAttribute("nobottomline") == "true") {
@@ -55,11 +62,44 @@ class WikiHeaderSection extends HTMLElement {
     }
 }
 
+class WikiDetails extends HTMLElement {
+    constructor() {
+        super();
+        this.insertAdjacentHTML("afterbegin", `<button><i class=\"arrow right\"></i> ${this.getAttribute("text")}</button>`);
+        this.addEventListener("click", this.handleDetailsBox);
+    }
+
+    handleDetailsBox() {
+        let wikiDetailsBox = this.getElementsByTagName("wiki-detailsbox")[0];
+        // determines if the wiki details box should be shown or not. it also determines the rotation of the arrow on the button.
+        if (wikiDetailsBox.style.display == "block") {
+            wikiDetailsBox.style.display = "none";
+            this.getElementsByTagName("button")[0].getElementsByTagName("i")[0].className = "arrow right";
+        }
+        else {
+            wikiDetailsBox.style.display = "block";
+            this.getElementsByTagName("button")[0].getElementsByTagName("i")[0].className = "arrow down";
+        }
+    }
+}
+
+class WikiDetailsBox extends HTMLElement {
+    constructor() {
+        super();
+    }
+}
+
 customElements.define("wiki-container", WikiContainer);
 customElements.define("wiki-page", WikiPage);
 customElements.define("wiki-section", WikiSection);
 customElements.define("wiki-subsection", WikiSubSection);
+
 customElements.define("wiki-tablecontent", WikiTableContent);
-customElements.define("wiki-tablecontentsection", WikiTableContentSection);
+customElements.define("wiki-infocontent", WikiInfoContent);
+customElements.define("wiki-contentsection", WikiContentSection);
+
 customElements.define("wiki-header", WikiHeader);
 customElements.define("wiki-headersection", WikiHeaderSection);
+
+customElements.define("wiki-details", WikiDetails);
+customElements.define("wiki-detailsbox", WikiDetailsBox);
